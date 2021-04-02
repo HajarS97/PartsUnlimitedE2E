1e053539b6c8fa34a467e1445bf6889bc9ae53ed
John Doe <johndoe@example.com>
2021-04-03 00:51:01 +0100

    firstcommit

--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
@@ -4,7 +4,7 @@ using System.ComponentModel.DataAnnotations;
 namespace PartsUnlimited.Models
 {
     public class CartItem : ILineItem
-    {
+    { //ñon preñier changement  
         [Key]
         public int CartItemId { get; set; }
 
